40e7d4f90a4b728c2ed7007c4112249f5bf38355
SsebasA <sebastian.antonio1707@gmail.com>
2024-10-12 12:56:19 -0600

    Init

---
+++ hack_banorte_reto/README.md
@@ -0,0 +1,3 @@
+# hack_banorte_reto
+
+A new Flutter project.
---
+++ hack_banorte_reto/android/app/src/main/kotlin/com/example/hack_banorte_reto/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hack_banorte_reto
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
